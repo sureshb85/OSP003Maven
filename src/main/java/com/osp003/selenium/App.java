76ab4c690b951b97f51ed823041437a24d54fe10
sureshb85 <sureshb85@yahoo.com>
2021-06-01 14:52:56 +0530

    Sample maven project

---
+++ src/main/java/com/osp003/selenium/App.java
@@ -0,0 +1,13 @@
+package com.osp003.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
